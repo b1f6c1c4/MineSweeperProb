c34605f6ecd4e87fb6839bf2d9f5de45c7a3a1eb
b1f6c1c4 <tujinzheng@126.com>
2015-08-25 23:59:22 +0800

    Simulator

---
+++ MineSweeperSimulator/MineSweeperSimulator.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "../MineSweeperSolver/GameMgr.h"
+#include <iostream>
+#include "../MineSweeperSolver/BinomialHelper.h"
+
+int main()
+{
+    CacheBinomials(30 * 16, 99);
+    while (true)
+    {
+        auto mgr = GameMgr(30, 16, 99);
+        mgr.Automatic();
+        std::cout << mgr.IsSucceed() << ":" << mgr.GetBits() << std::endl;
+    }
+    system("pause");
+    return 0;
+}
