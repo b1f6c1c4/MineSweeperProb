9a88e2b8107b6f1e4e91e72d43bc05d85ca0aeb5
b1f6c1c4 <tujinzheng@126.com>
2015-08-26 23:46:51 +0800

    取消ThreadPool

--- MineSweeperSimulator/MineSweeperSimulator.cpp
+++ MineSweeperSimulator/MineSweeperSimulator.cpp
@@ -2,73 +2,92 @@
 #include <iomanip>
 #include <iostream>
 #include <fstream>
+#include <thread>
+#include <mutex>
 #include "../MineSweeperSolver/GameMgr.h"
 #include "../MineSweeperSolver/BinomialHelper.h"
-#include "ThreadPool.h"
+
+std::mutex mtx;
+std::vector<std::thread> m_Threads;
+volatile int rest;
+volatile auto succeed = 0, total = 0;
+
+void Process()
+{
+    while (true)
+    {
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            if (rest == 0)
+                break;
+            --rest;
+        }
+
+        auto mgr = GameMgr(30, 16, 99);
+        mgr.Automatic();
+
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            if (mgr.GetSucceed())
+                ++succeed;
+            ++total;
+        }
+    }
+}
 
 int main()
 {
     CacheBinomials(30 * 16, 99);
 
-    int thrs, rpM;
+    int thrs, rpM, wait;
     std::ifstream fin("config.txt");
-    fin >> thrs >> rpM;
+    fin >> thrs >> wait >> rpM;
     fin.close();
 
-    std::cout << thrs << " " << rpM << std::endl;
+    std::cout << thrs << " " << wait << " " << rpM << std::endl;
+
+    rest = rpM;
 
     std::ofstream fout("output.txt", std::ios::app);
         
-    std::mutex mtx;
     auto succeedT = 0, totalT = 0;
-    volatile auto succeed = 0, total = 0;
 
+    while (thrs-- > 0)
+        m_Threads.emplace_back(&Process);
+
+    auto i = 0;
+    while (rest > 0)
     {
-        ThreadPool pool(thrs);
-        for (auto i = 0; i < rpM; ++i)
-            pool.enqueue([&succeed, &total, &mtx]()
-                         {
-                             auto t = 0, s = 0;
-                             //for (auto n = 0; n < rpN; ++n)
-                             {
-                                 auto mgr = GameMgr(30, 16, 99);
-                                 mgr.Automatic();
-                                 ++t;
-                                 if (mgr.GetSucceed())
-                                     ++s;
-                             }
-                             mtx.lock();
-                             succeed += s;
-                             total += t;
-                             mtx.unlock();
-                         });
+        std::this_thread::sleep_for(std::chrono::seconds{ wait });
 
-        auto i = 0;
-        while (totalT + total < rpM)
-        {
-            std::this_thread::sleep_for(std::chrono::seconds{1});
+        i += wait;
+        mtx.lock();
+        if (total > 0)
+            fout << succeed << " " << total << std::endl << std::flush;
+        succeedT += succeed;
+        totalT += total;
+        succeed = 0;
+        total = 0;
+        mtx.unlock();
 
-            if (++i > 60)
-            {
-                i = 0;
-                mtx.lock();
-                if (total > 0)
-                    fout << succeed << " " << total << std::endl << std::flush;
-                succeedT += succeed;
-                totalT += total;
-                succeed = 0;
-                total = 0;
-                mtx.unlock();
-            }
-
-            auto est = totalT + total == 0 ? 0 : i * rpM / (totalT + total) - i;
-            auto estMin = est / 60 % 60;
-            auto estHour = est / 60 / 60;
-            std::cout << succeedT + succeed << " / " << totalT + total << " " << estHour << ":" << estMin << ":" << est << " " << static_cast<double>(succeedT + succeed) / (totalT + total) << std::endl;
-        }
+        auto est = static_cast<int>(totalT == 0 ? 0 : static_cast<double>(i) * rpM / totalT - i);
+        auto estMin = est / 60 % 60;
+        auto estHour = est / 60 / 60;
+        std::cout << succeedT << " / " << totalT << " " << estHour << ":" << estMin << ":" << est << " " << static_cast<double>(succeedT) / (totalT) << std::endl;
+        if (est < wait)
+            break;
     }
-    fout << succeed << " " << total << std::endl << std::flush;
+
+    for (auto &th : m_Threads)
+        th.join();
+
+    if (total > 0)
+        fout << succeed << " " << total << std::endl << std::flush;
     fout.close();
+
+    succeedT += succeed;
+    totalT += total;
+    std::cout << succeedT << " / " << totalT << " " << static_cast<double>(succeedT) / (totalT) << std::endl;
 
     system("pause");
     return 0;
