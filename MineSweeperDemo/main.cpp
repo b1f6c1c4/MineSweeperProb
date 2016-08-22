8165ac6768a2d95b031ad90a3cec283b2a64d4f7
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-08-23 00:13:12 +0800

    simplify cli; update gaclib

--- MineSweeperDemo/main.cpp
+++ MineSweeperDemo/main.cpp
@@ -2,13 +2,10 @@
 #include <Windows.h>
 #include <vector>
 #include "../MineSweeperSolver/BinomialHelper.h"
-#include <string>
 #include "MineSweeperDemo.h"
-#include "boost/program_options.hpp"
-#include <iostream>
 #include "ReadStrategy.h"
 
-static size_t interval = 100;
+static int interval = 50;
 static size_t width, height, totalMines;
 static std::shared_ptr<Strategy> strategy;
 
@@ -17,50 +14,13 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     SetProcessDPIAware();
 
     std::string hsh;
+    if (strlen(lpCmdLine) == 0)
+        hsh = "FL@[1,1]-PSEQ-D128-30-16-T99-NR";
+    else
+        hsh = lpCmdLine;
 
-
-    namespace po = boost::program_options;
-    po::options_description desc("Options");
-    desc.add_options()
-        ("help,h", "Print help messages")
-        ("interval,i", po::value<size_t>(&interval), "Time interval within two moves")
-        ("CONFIG", po::value<std::string>(&hsh), "Configuration");
-
-    po::positional_options_description positionalOptions;
-    positionalOptions.add("CONFIG", 1);
-
-    po::variables_map vm;
-    try
-    {
-        std::vector<std::string> args;
-        args.emplace_back(lpCmdLine);
-        po::store(
-                  po::command_line_parser(args)
-                  .options(desc)
-                  .positional(positionalOptions).run(),
-                  vm);
-
-        if (vm.count("help"))
-        {
-            std::cout << "MineSweeperDemo" << std::endl;
-            std::cout << desc << std::endl;
-            return 0;
-        }
-
-        po::notify(vm);
-
-        if (hsh.length() == 0)
-            hsh = "FL@[1,1]-PSEQ-D256-30-16-T99-NR";
-
-        strategy = std::make_shared<Strategy>();
-        ReadStrategy(hsh, *strategy, width, height, totalMines);
-    }
-    catch (std::exception &e)
-    {
-        std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
-        std::cerr << desc << std::endl;
-        return 1;
-    }
+    strategy = std::make_shared<Strategy>();
+    ReadStrategy(hsh, *strategy, width, height, totalMines);
 
     CacheBinomials(width * height, totalMines);
 
