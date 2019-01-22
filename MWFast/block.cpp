40adc18827ed6f449741278ea1f4003f57ecedc2
b1f6c1c4 <b1f6c1c4@gmail.com>
2019-01-22 14:38:53 -0500

    block

---
+++ MWFast/block.cpp
@@ -0,0 +1,94 @@
+#include "block.h"
+
+blk_t::blk_t() : value_(0) { }
+
+blk_t::blk_t(const uint8_t v) : value_(v) { }
+
+uint8_t blk_t::operator()() const
+{
+	return value_;
+}
+
+bool blk_t::is_front() const
+{
+	return value_ & 0x40;
+}
+
+blk_t &blk_t::set_front(const bool v)
+{
+	value_ &= ~0x40;
+	value_ |= v ? 0x40 : 0x00;
+	return *this;
+}
+
+bool blk_t::is_closed() const
+{
+	return value_ & 0x20;
+}
+
+blk_t &blk_t::set_closed(const bool v)
+{
+	value_ &= ~0x20;
+	value_ |= v ? 0x20 : 0x00;
+	return *this;
+}
+
+bool blk_t::is_mine() const
+{
+	return value_ & 0x10;
+}
+
+blk_t &blk_t::set_mine(const bool v)
+{
+	value_ &= ~0x10;
+	value_ |= v ? 0x10 : 0x00;
+	return *this;
+}
+
+uint8_t blk_t::neighbor() const
+{
+	return value_ & 0x0f;
+}
+
+blk_t &blk_t::set_neighbor(const uint8_t v)
+{
+	value_ &= ~0x40;
+	value_ |= v;
+	return *this;
+}
+
+blk_t blk_t::closed_mine()
+{
+	return 0x30;
+}
+
+blk_t blk_t::closed_simple(const uint8_t v)
+{
+	return 0x20 | v;
+}
+
+std::ostream &operator<<(std::ostream &os, const blk_t &b)
+{
+	if (!b.is_closed())
+	{
+		if (b.is_mine())
+			os << 'M';
+		else
+			os << static_cast<size_t>(b.neighbor());
+	}
+	else if (b.is_front())
+	{
+		if (b.is_mine())
+			os << '+';
+		else
+			os << '-';
+	}
+	else
+	{
+		if (b.is_mine())
+			os << '.';
+		else
+			os << ' ';
+	}
+	return os;
+}
---
+++ MWFast/block.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "common.h"
+
+class blk_t
+{
+public:
+	blk_t();
+	// ReSharper disable once CppNonExplicitConvertingConstructor
+	blk_t(uint8_t v);
+	uint8_t operator()() const;
+
+	static blk_t closed_mine();
+	static blk_t closed_simple(uint8_t);
+
+	bool is_front() const;
+	blk_t &set_front(bool);
+
+	bool is_closed() const;
+	blk_t &set_closed(bool);
+
+	bool is_mine() const;
+	blk_t &set_mine(bool);
+
+	uint8_t neighbor() const;
+	blk_t &set_neighbor(uint8_t);
+
+	friend std::ostream &operator<<(std::ostream &os, const blk_t &b);
+private:
+	uint8_t value_;
+};
