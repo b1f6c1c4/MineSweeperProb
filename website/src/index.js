520a0756043f214cdd5937a780e942d561619bc0
b1f6c1c4 <b1f6c1c4@gmail.com>
2022-10-10 20:45:58 -0400

    WIP: index and deps

--- website/src/index.js
+++ website/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
